28ad6a367cfe3522c535573deafc447691444e6d
Léo NOEL <leo.noel.fr@gmail.com>
2015-08-27 11:53:58 +0200

    fixed fripon dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.28"]
-                 [fripon "0.0-3-SNAPSHOT"]])
+                 [fripon "0.0-3"]])
